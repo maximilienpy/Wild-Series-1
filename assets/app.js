7a88ac1edba3f106435c2fb6c3157c7e4d327e36
BastienGoze <bastien.goze34@gmail.com>
2020-11-22 11:52:25 +0100

    quest 4

--- assets/app.js
+++ assets/app.js
@@ -19,12 +19,8 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+// // or you can include specific pieces
+// // require('bootstrap/js/dist/tooltip');
+// // require('bootstrap/js/dist/popover');
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
